d09ca14f42d527438aaf4f16721c4d32b4c08644
fun841 <abdirovanaz@gmail.com>
2025-07-01 18:32:43 +0600

    2task.cpp

--- Lab5.pp1/2task.cpp
+++ Lab5.pp1/2task.cpp
@@ -7,14 +7,10 @@ int main() {
     stack<char> s;
 
     cout << "Enter a string: ";
-    getline(cin, input);  // читаем всю строку, включая пробелы
-
-    // Кладём каждый символ строки в стек
+    getline(cin, input); 
     for (int i = 0; i < input.length(); i++) {
         s.push(input[i]);
     }
-
-    // Извлекаем символы из стека и формируем перевёрнутую строку
     string reversed = "";
     while (!s.empty()) {
         reversed += s.top();
